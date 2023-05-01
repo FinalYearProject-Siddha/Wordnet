5f87c5ad0e5b225bd56e96c095a13858aa71eead
Shreya Sriram <76148126+shreya1110-dev@users.noreply.github.com>
2023-05-01 09:24:09 +0530

    Added support for English translation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from googletrans import Translator, constants
 import json
 from json2html import *
 
@@ -23,8 +24,23 @@ def results():
             return render_template('index.html', msg="Please enter valid text")
         if data.strip() not in rels.keys():
             return render_template('not_found.html', show = "Oh no! The requested resource isn't available!")
-        table_html = json2html.convert(json = rels[data.strip()], table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
-        return render_template('results.html', show = table_html, term = data.strip())
+        json_data = rels[data.strip()]
+        tamil_json_data = {}
+        translator = Translator()
+        for (k,v) in json_data.items():
+            if k=='Category':
+                translation = translator.translate(v, src="ta")
+                tamil_json_data[k] = translation.text
+            else:
+                for value in json_data[k]:
+                    translation = translator.translate(value, src="ta")
+                    if k in tamil_json_data.keys():
+                        tamil_json_data[k].append(translation.text)
+                    else:
+                        tamil_json_data[k] = [translation.text]
+        table_html = json2html.convert(json = json_data, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+        tamil_table_html = json2html.convert(json = tamil_json_data, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+        return render_template('results.html', json = table_html, tamil_json = tamil_table_html, term = data.strip(), tamil_term = translator.translate(data.strip()).text)
     
 
 if __name__ == '__main__':
